e1aabaeda8780dabb095bc6cb250fc315be13c38
apainintheneck <apainintheneck@gmail.com>
2022-03-16 15:00:21 -0700

    Initial Commit

---
+++ shell-lib/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  shell-lib
+//
+//  Created by Kevin on 3/16/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+   // insert code here...
+   std::cout << "Hello, World!\n";
+   return 0;
+}
