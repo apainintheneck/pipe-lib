30f7831b67100af18ccf9ba24ad9358db61e907a
apainintheneck <apainintheneck@gmail.com>
2022-03-17 20:44:57 -0700

    Added sort, chmod, and test commands

--- shell-lib/sh.hpp
+++ shell-lib/sh.hpp
@@ -13,6 +13,7 @@
 #include <iostream>
 #include <string>
 #include <filesystem>
+#include <vector>
 
 namespace sh {
 
@@ -23,19 +24,25 @@ path pwd() noexcept {
    return std::filesystem::current_path( err );
 }
 
+bool cd() noexcept {
+   std::error_code err;
+   std::filesystem::current_path( std::getenv("HOME"), err );
+   return !err;
+}
+
 bool cd(const path& dest) noexcept {
    std::error_code err;
    std::filesystem::current_path( dest, err );
    return !err;
 }
 
-bool mv(const path& src, const path& dest) noexcept {
+bool mv( const path& src, const path& dest ) noexcept {
    std::error_code err;
    std::filesystem::rename( src, dest, err );
    return !err;
 }
 
-bool rm(const path& src, const bool recursive = true) noexcept {
+bool rm( const path& src, const bool recursive = true ) noexcept {
    std::error_code err;
    if(recursive) {
       std::filesystem::remove_all( src, err );
@@ -45,7 +52,7 @@ bool rm(const path& src, const bool recursive = true) noexcept {
    }
 }
 
-bool cp(const path& src, const path& dest, const bool recursive = true) noexcept {
+bool cp( const path& src, const path& dest, const bool recursive = true ) noexcept {
    std::error_code err;
    if(recursive) {
       std::filesystem::copy( src, dest, std::filesystem::copy_options::recursive, err );
@@ -55,13 +62,13 @@ bool cp(const path& src, const path& dest, const bool recursive = true) noexcept
    }
 }
 
-bool mkdir(const path& dir) noexcept {
+bool mkdir( const path& dir ) noexcept {
    std::error_code err;
    return std::filesystem::create_directory( dir, err );
 }
 
-bool rmdir(const path& dir) noexcept {
-   if(!std::filesystem::is_directory(dir) || std::filesystem::is_empty(dir))
+bool rmdir( const path& dir ) noexcept {
+   if( !std::filesystem::is_directory( dir ) || std::filesystem::is_empty( dir ) )
       return false;
    
    std::error_code err;
@@ -90,8 +97,8 @@ bool cat(const std::string& filename, std::ostream& os = std::cout) {
    return false;
 }
 
-void echo(const char* input) noexcept {
-   std::puts( input );
+bool  echo(const char* input) noexcept {
+   return std::puts(input) >= 0;
 }
 
 class tee {
@@ -120,16 +127,39 @@ bool touch(const std::string& filepath) noexcept {
    return file.is_open();
 }
 
+//It'd be great to find a better way to alias dirname and basename since they're identical.
 std::string dirname(path filepath) {
    filepath.remove_filename();
    return filepath;
 }
 
-std::string basename(const path& filepath) {
-   return dirname(filepath);
+std::string basename(path filepath) {
+   filepath.remove_filename();
+   return filepath;
 }
 
-bool uniq(const std::string& filepath, std::ostream& os = std::cout) {
+//Remember to benchmark the puts version. If it's not faster, delete it.
+bool uniq(const std::string& filepath) {
+   std::ifstream file(filepath);
+   if(!file.is_open())
+      return false;
+   
+   std::string prev;
+   if(std::getline(file, prev))
+      std::puts(prev.c_str());
+   
+   std::string buffer;
+   while(std::getline(file, buffer)) {
+      if(buffer == prev) continue;
+      
+      std::puts(buffer.c_str());
+      prev = std::move(buffer);
+   }
+   
+   return true;
+}
+
+bool uniq(const std::string& filepath, std::ostream& os) {
    std::ifstream file(filepath);
    if(!file.is_open())
       return false;
@@ -149,13 +179,100 @@ bool uniq(const std::string& filepath, std::ostream& os = std::cout) {
    return true;
 }
 
+//Remember to benchmark the puts version. If it's not faster, delete it.
+//Also, it might be worth exploring heap sort since you're getting one line at a time.
+bool sort(const std::string& filepath) {
+   std::ifstream file(filepath);
+   if(!file.is_open())
+      return false;
+   
+   std::string buffer;
+   std::vector<std::string> lines;
+   while(std::getline(file, buffer)) {
+      lines.push_back(buffer);
+   }
+   
+   for(const auto& line: lines)
+      std::puts(line.c_str());
+   
+   return true;
+}
+
+bool sort(const std::string& filepath, std::ostream& os) {
+   std::ifstream file(filepath);
+   if(!file.is_open())
+      return false;
+   
+   std::string buffer;
+   std::vector<std::string> lines;
+   while(std::getline(file, buffer)) {
+      lines.push_back(buffer);
+   }
+   
+   for(const auto& line: lines)
+      os << line;
+   
+   return true;
+}
+
+std::filesystem::file_type file(const path& filepath) noexcept {
+   std::error_code err;
+   const auto status = std::filesystem::status(filepath, err);
+   return status.type();
+}
+
+//Add a more intuitive way to add arguments
+bool chmod(const path& filepath, const std::filesystem::perms permissions) noexcept {
+   std::error_code err;
+   std::filesystem::permissions(filepath, permissions, err);
+   return !err;
+}
+
+namespace test {
+
+bool b(const path& file) noexcept {
+   std::error_code err;
+   return std::filesystem::is_block_file(file, err) && !err;
+}
+
+bool c(const path& file) noexcept {
+   std::error_code err;
+   return std::filesystem::is_character_file(file, err) && !err;
+}
+
+bool d(const path& file) noexcept {
+   std::error_code err;
+   return std::filesystem::is_directory(file, err) && !err;
+}
+
+bool f(const path& file) noexcept {
+   std::error_code err;
+   return std::filesystem::is_regular_file(file, err) && !err;
+}
+
+bool p(const path& file) noexcept {
+   std::error_code err;
+   return std::filesystem::is_fifo(file, err) && !err;
+}
+
+bool L(const path& file) noexcept {
+   std::error_code err;
+   return std::filesystem::is_symlink(file, err) && !err;
+}
+
+bool S(const path& file) noexcept {
+   std::error_code err;
+   return std::filesystem::is_socket(file, err) && !err;
+}
+
+} //namespace test
+
 //size_t wc(std::istream& input) {
 //
 //}
 
 /*
  alias
- chmod
  chown
  cksum
  cmp
@@ -166,7 +283,6 @@ bool uniq(const std::string& filepath, std::ostream& os = std::cout) {
  date
  expr
  fold
- file
  find
  getopts
  grep
@@ -180,7 +296,6 @@ bool uniq(const std::string& filepath, std::ostream& os = std::cout) {
  paste
  read
  sed
- sort
  tail
  test
  time
@@ -193,4 +308,4 @@ bool uniq(const std::string& filepath, std::ostream& os = std::cout) {
  symlink - don't know what it's called
  */
 
-}
+} //namespace sh
